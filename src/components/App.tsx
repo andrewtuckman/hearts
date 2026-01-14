ba07bfd037da48f20a2016e1fba4d980e956592c
Andrew Tuckman <75922332+andrewtuckman@users.noreply.github.com>
2026-01-13 19:47:54 -0500

    clean up app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,16 +6,9 @@ function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Card suit="hearts" rank="K"></Card>
         <Card suit="hearts" rank="Q"></Card>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Card suit="hearts" rank="J"></Card>
       </header>
     </div>
   );
