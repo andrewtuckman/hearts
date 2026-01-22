b546ef2ac24dc9940e58febeceddb578ac83c17c
Andrew Tuckman <75922332+andrewtuckman@users.noreply.github.com>
2026-01-21 21:42:50 -0500

    add cardback image

--- src/components/Card/CardBack.tsx
+++ src/components/Card/CardBack.tsx
@@ -6,7 +6,11 @@ interface CardBackProps {
 }
 
 export const CardBack: React.FC<CardBackProps> = ({ size = 'normal' }) => {
-  return <div className={`card-back card-back--${size}`} />;
+  return (
+    <div className={`card-back card-back--${size}`}>
+      <img src="/cards/cardback.png" alt="Card back" draggable={false} />
+    </div>
+  );
 };
 
 export default CardBack;
