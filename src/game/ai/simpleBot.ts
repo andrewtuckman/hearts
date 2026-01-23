ab0929a9dc71665b252e6e8862bfd313691ea32e
Andrew Tuckman <75922332+andrewtuckman@users.noreply.github.com>
2026-01-22 20:07:24 -0500

    random dumb bot

--- src/game/ai/simpleBot.ts
+++ src/game/ai/simpleBot.ts
@@ -1,1 +1,8 @@
-export {};
+import { HandCard } from '../../models/types';
+import { getRandomInt } from '../../utils/utils';
+
+export const simpleBot = (hand: HandCard[]): HandCard => {
+  // Pick a random card from the hand
+  const randomIndex = getRandomInt(0, hand.length - 1);
+  return hand[randomIndex];
+};
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -12,3 +12,7 @@ export function createPlayerDict(initValue: any): Record<PID, any> {
     return accum;
   }, {} as Record<PID, any>);
 }
+
+export function getRandomInt(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
